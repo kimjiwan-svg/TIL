403ed87ea326f4704e077c2220677d92fb80b636
kimjiwan-svg <uridle5@naver.com>
2024-01-31 09:53:15 +0900

    feat: Read Password letter
    - letter is written with morse code
    - read it with given codes

---
+++ day3/morse.py
@@ -0,0 +1,15 @@
+def solution(letter):
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    letter_list = letter.split(' ')
+    answer = ''
+    for i in letter_list:
+        alpha = morse[i]
+        answer += alpha
+    
+    return answer
